da51c0008fe0cf3ba28d5eb7c04e5161930463df
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-10-18 15:09:08 +0530

    second commit

---
+++ database/migrations/2025_10_18_093343_alter_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            // Add the new columns
+            $table->string('username')->unique()->after('password');
+            $table->enum('order_views', ['self', 'global'])->default('self')->after('username');
+            $table->enum('change_status', ['0', '1'])->default('0')->after('order_views');
+            
+            // Make the email column nullable
+            $table->string('email')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2025_10_18_093639_create_t_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('t_clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('category');
+            $table->unsignedBigInteger('sub_category');
+            $table->text('tags');
+            $table->string('city');
+            $table->string('state');
+            $table->unsignedBigInteger('rm');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('t_clients');
+    }
+};
