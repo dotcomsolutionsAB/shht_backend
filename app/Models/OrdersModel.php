4589a9efa7054b506558485f4424ab4baa7e2d73
devbygongo <gsample7894@google.com>
2025-10-23 12:49:43 +0530

    update

--- app/Models/OrdersModel.php
+++ app/Models/OrdersModel.php
@@ -9,4 +9,29 @@ class OrdersModel extends Model
     //
     protected $table = 't_orders';
     protected $fillable = ['client', 'client_contact_person', 'so_no', 'so_date', 'order_no', 'order_date', 'invoice', 'status', 'initiated_by', 'checked_by', 'dispatched_by', 'drive_link'];
+
+    public function clientRef()
+    {
+        return $this->belongsTo(ClientsModel::class, 'client', 'id');
+    }
+
+    public function contactRef()
+    {
+        return $this->belongsTo(ClientsContactPersonModel::class, 'client_contact_person', 'id');
+    }
+
+    public function initiatedByRef()
+    {
+        return $this->belongsTo(User::class, 'initiated_by', 'id');
+    }
+
+    public function checkedByRef()
+    {
+        return $this->belongsTo(User::class, 'checked_by', 'id');
+    }
+
+    public function dispatchedByRef()
+    {
+        return $this->belongsTo(User::class, 'dispatched_by', 'id');
+    }
 }
