e245f366917fad249f651212aa3e44213400c0db
Soumya Gangopadhyay <gsample7894.sg6@gmail.com>
2025-10-25 14:36:29 +0530

    fix

--- routes/api.php
+++ routes/api.php
@@ -89,7 +89,7 @@ Route::middleware('auth:sanctum', 'role:admin,sales,staff,dispatch')->group(func
         Route::post('/update/{id}', [OrdersController::class, 'edit']);
         Route::delete('/delete/{id}', [OrdersController::class, 'delete']);
         Route::get('/get_order_status/{id}', [OrdersController::class, 'validate_order_status']);
-        Route::get('/changeStatus', [OrdersController::class, 'updateStatus']);
+        Route::post('/changeStatus', [OrdersController::class, 'updateStatus']);
     });
 
     // invoice route
