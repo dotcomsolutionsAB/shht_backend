8aaec7606de53e96809b940aa98325fd2d41c746
devbygongo <gsample7894.sg6@gmail.com>
2025-12-08 20:25:19 +0530

    update and fix

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\OrdersModel;
 use App\Models\CounterModel;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
